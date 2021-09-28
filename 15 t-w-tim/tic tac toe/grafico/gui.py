ad438d16607333c3c236b5425fbc01e84d1d5422
jorge gago <jorgegago2526@gmail.com>
2021-09-28 13:31:32 +0200

    creacion basica de graficos

---
+++ 15 t-w-tim/tic tac toe/grafico/gui.py
@@ -0,0 +1,17 @@
+import tkinter
+
+def icons(raiz): #cargo los iconos
+    pass
+
+def selcion_icon(): # player seleciona su icono
+    pass
+
+def tablero(raiz): #dibuja el tablero del juego
+    pass
+
+def mensaje(raiz, st): # muestra mensaje de victoria o otra info
+    pass
+
+def todo(raiz): # frame principal para tablero nombre y puntuacion
+    pass
+
